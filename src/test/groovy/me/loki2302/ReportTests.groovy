3a40460d447b9998246493b6f8486527ddf064b6
loki2302 <loki2302@loki2302.me>
2015-07-20 23:05:30 +0300

    Updated dependencies, reimported the data

--- src/test/groovy/me/loki2302/ReportTests.groovy
+++ src/test/groovy/me/loki2302/ReportTests.groovy
@@ -27,55 +27,33 @@ class ReportTests {
     void topRepositoriesByCommitCountWorks() {
         def topRepositoriesByCommitCountReport = new TopRepositoriesByCommitCountReport()
         def rows = topRepositoriesByCommitCountReport.make(sql, 3)
-        assertEquals('docpad-experiment', rows[0].name)
-        assertEquals(121, rows[0].commits)
-        assertEquals('spring-web-app-experiment', rows[1].name)
-        assertEquals(102, rows[1].commits)
-        assertEquals('nodejs-app-experiment', rows[2].name)
-        assertEquals(93, rows[2].commits)
+        assertEquals('nodejs-angular-app', rows[0].name)
+        assertEquals(275, rows[0].commits)
+        assertEquals('nodejs-app-experiment', rows[1].name)
+        assertEquals(213, rows[1].commits)
+        assertEquals('nodejs-experiment', rows[2].name)
+        assertEquals(177, rows[2].commits)
     }
 
     @Test
     void repositoriesWithFirstAndLastCommitDatesReportWorks() {
         def repositoriesWithFirstAndLastCommitDatesReport = new RepositoriesWithFirstAndLastCommitDatesReport()
         def rows = repositoriesWithFirstAndLastCommitDatesReport.make(sql)
-        assertEquals('DotNetOpenAuth.GoogleOAuth2', rows[0].name)
-        assertEquals(Timestamp.valueOf('2013-02-28 23:55:11.0'), rows[0].firstCommitDate)
-        assertEquals(Timestamp.valueOf('2013-07-07 00:35:00.0'), rows[0].lastCommitDate)
-        assertEquals('abstract-mvc-experiment', rows[1].name)
-        assertEquals(Timestamp.valueOf('2013-12-04 10:24:24.0'), rows[1].firstCommitDate)
-        assertEquals(Timestamp.valueOf('2013-12-04 10:50:00.0'), rows[1].lastCommitDate)
-        assertEquals('android-custom-layout-loader-experiment', rows[2].name)
-        assertEquals(Timestamp.valueOf('2013-11-28 09:05:12.0'), rows[2].firstCommitDate)
-        assertEquals(Timestamp.valueOf('2013-11-28 09:12:24.0'), rows[2].lastCommitDate)
+        assertEquals('analytics-experiment', rows[0].name)
+        assertEquals(Timestamp.valueOf('2014-11-12 10:50:05.0'), rows[0].firstCommitDate)
+        assertEquals(Timestamp.valueOf('2014-11-19 17:42:46.0'), rows[0].lastCommitDate)
     }
 
     @Test
     void lastCommitsByRepositoryReportWorks() {
         def lastCommitsByRepositoryReport = new LastCommitsByRepositoryReport()
         def repositories = lastCommitsByRepositoryReport.make(sql, 3)
-        assertEquals('DotNetOpenAuth.GoogleOAuth2', repositories[0].name)
-        assertEquals(Timestamp.valueOf('2013-07-07 00:35:00.0'), repositories[0].commits[0].date)
-        assertEquals('e9a120c2e975a61199e63756baec0aed995a316a', repositories[0].commits[0].sha)
-        assertEquals(Timestamp.valueOf('2013-03-14 21:41:40.0'), repositories[0].commits[1].date)
-        assertEquals('c66347bd8843df7bf83cd951cb5d68d3363854b2', repositories[0].commits[1].sha)
-        assertEquals(Timestamp.valueOf('2013-03-14 21:36:07.0'), repositories[0].commits[2].date)
-        assertEquals('be60f6f741687e5bdce7009fdf0b3c3f9b6480b7', repositories[0].commits[2].sha)
-
-        assertEquals('abstract-mvc-experiment', repositories[1].name)
-        assertEquals(Timestamp.valueOf('2013-12-04 10:50:00.0'), repositories[1].commits[0].date)
-        assertEquals('b7e0439790694e15a74e835371668382f16e41a4', repositories[1].commits[0].sha)
-        assertEquals(Timestamp.valueOf('2013-12-04 10:47:00.0'), repositories[1].commits[1].date)
-        assertEquals('480319c6018f922df912ef3c0d2442e849a07f7a', repositories[1].commits[1].sha)
-        assertEquals(Timestamp.valueOf('2013-12-04 10:25:28.0'), repositories[1].commits[2].date)
-        assertEquals('856724c4e456ad3cd7bfac7cc8d3b9525bbf702c', repositories[1].commits[2].sha)
-
-        assertEquals('android-custom-layout-loader-experiment', repositories[2].name)
-        assertEquals(Timestamp.valueOf('2013-11-28 09:12:24.0'), repositories[2].commits[0].date)
-        assertEquals('5805b9da60cd156ba162e65f3b2b719de62feacc', repositories[2].commits[0].sha)
-        assertEquals(Timestamp.valueOf('2013-11-28 09:05:38.0'), repositories[2].commits[1].date)
-        assertEquals('04d7bc6c58da42e2d3dfbabb36300697ff9b3023', repositories[2].commits[1].sha)
-        assertEquals(Timestamp.valueOf('2013-11-28 09:05:12.0'), repositories[2].commits[2].date)
-        assertEquals('b87131004535c3dcfca2b1ef8c3455ebc73b6ece', repositories[2].commits[2].sha)
+        assertEquals('analytics-experiment', repositories[0].name)
+        assertEquals(Timestamp.valueOf('2014-11-19 17:42:46.0'), repositories[0].commits[0].date)
+        assertEquals('c77e7b32e3d30e7d082981116b86db96e2af0300', repositories[0].commits[0].sha)
+        assertEquals(Timestamp.valueOf('2014-11-19 17:33:17.0'), repositories[0].commits[1].date)
+        assertEquals('454c71a08137436efae61ba26e6d767db3ef3ffb', repositories[0].commits[1].sha)
+        assertEquals(Timestamp.valueOf('2014-11-14 10:15:14.0'), repositories[0].commits[2].date)
+        assertEquals('a325e4e3fdb8336011aab60c344d64b94d9f8396', repositories[0].commits[2].sha)
     }
 }
